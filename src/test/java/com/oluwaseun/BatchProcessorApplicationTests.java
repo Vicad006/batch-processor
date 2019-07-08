8ac436043aebd5cafc8a243dddcadc277c76c713
Vicad006 <vicad006@yaoo.com>
2019-07-08 12:54:58 -0400

    Setup Spring Boot CSV Batch Processor

---
+++ src/test/java/com/oluwaseun/BatchProcessorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oluwaseun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BatchProcessorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
